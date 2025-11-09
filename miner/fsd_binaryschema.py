140d5dafe63d05767fc28c8ee2f21de9924debe6
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-11-09 10:22:44 +0100

    Comment print of each solar system id

--- miner/fsd_binaryschema.py
+++ miner/fsd_binaryschema.py
@@ -189,7 +189,7 @@ class FsdBinaryMiner(BaseMiner):
             for item in (pre_fsd_data.items()):
                 #fsd_json.append(str("FSD_ENTRY: ") + str(item[0]))
 
-                print(item[0])
+                # print(item[0])
                 for items in item:
                     #print(items)
                     if type(items) == objectLoader.ObjectLoader:
